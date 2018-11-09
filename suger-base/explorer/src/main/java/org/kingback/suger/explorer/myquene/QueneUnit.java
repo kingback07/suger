b98519e2e15f11328b6c07222ec86e408376649b
kingback <kingback_07@126.com>
2018-11-09 19:22:54 +0800

    阻塞队列demo

---
+++ suger-base/explorer/src/main/java/org/kingback/suger/explorer/myquene/QueneUnit.java
@@ -0,0 +1,49 @@
+package org.kingback.suger.explorer.myquene;
+
+import java.util.List;
+
+/**
+ * 模拟一个阻塞队列
+ * 超过20条记录时写阻塞
+ * 无数据时读阻塞
+ */
+public class QueneUnit {
+
+    private List<Object> _ds;
+
+    private int maxCount = 20;
+
+    private volatile int curCount = 0;
+
+    private final Object readKey=new Object();
+    private final Object writeKey=new Object();
+
+    public Object getObj() {
+        try {
+            if (curCount == 0) {
+                readKey.wait();
+            }
+            Object res=_ds.get(curCount);
+            curCount--;
+            return res;
+        } catch (Exception ex) {
+            return null;
+        } finally {
+        }
+    }
+
+    public void setObj(Object val) {
+        try{
+            if(curCount>=maxCount){
+                writeKey.wait();
+            }
+            _ds.add(val);
+            curCount++;
+            readKey.notify();
+        }catch (Exception ex){
+
+        }finally {
+        }
+    }
+
+}
