29d27e8b8458f4d79a66b97a9fedd758153b4db7
kingback <kingback_07@126.com>
2020-06-04 00:04:54 +0800

    数组简单排序法

---
+++ suger-base/learning/src/test/java/org/kingback/suger/learning/LearningApplicationTests.java
@@ -0,0 +1,35 @@
+package org.kingback.suger.learning;
+
+import org.junit.Test;
+import org.kingback.suger.learning.datastructures.array.ArrayOrderSimple;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class LearningApplicationTests {
+
+    private void printArray(int[] array){
+        StringBuilder sb=new StringBuilder();
+        sb.append("[");
+        for(int i=0;i<array.length-1;i++){
+            sb.append(array[i]+",");
+        }
+        sb.append(array[array.length-1]+"]");
+        System.out.println(sb.toString());
+    }
+
+    @Test
+    public void arrayOrderByInsertTest(){
+        int[] array=new int[]{10,5,9,6,2,3,8,7,1,4};
+        ArrayOrderSimple.orderByInsert(array);
+        printArray(array);
+    }
+
+
+    @Test
+    public void arrayOrderByBubbleTest(){
+        int[] array=new int[]{10,5,9,6,2,3,8,7,1,4};
+        ArrayOrderSimple.orderByBubble(array);
+        printArray(array);
+    }
+
+}
